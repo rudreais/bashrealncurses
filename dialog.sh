c05ef8c19f991eb6c25bb6de3bfcc9cfba14ac25
valentin.lattay <valentin.lattay@epitech.eu>
2018-02-25 02:07:42 +0100

    structures

--- dialog.sh
+++ dialog.sh
@@ -3,27 +3,68 @@
 lines=$(tput lines)
 cols=$(tput cols)
 mid=()
-size=15
+colors=("red" "green" "yellow" "blue" "magenta" "cyan" "lgrey")
+
 
 mid+=($(echo "$lines / 2" | bc))
-mid+=($(echo "$cols / 2 - $size" | bc))
+mid+=($(echo "$cols / 2" | bc))
 
-function main {
-        function bluescreen {
-                for ((i=0; i<=lines; i++))
-                do
-                        printf "\033[44m\033[K\033[0m\n"
+function is_in {
+        index=1
+        for i in "${colors[@]}"
+        do
+                if [ "$1" == "$i" ]
+                then
+                        echo "$(echo "40 + $index" | bc)m"
+                        return
+                fi
+                index=$(echo "$index + 1" | bc)
+        done
+        echo "44m"
+}
+
+function inputs {
+        function arrow_input {
+                while read -rsn1 ui; do
+                case "$ui" in
+                $'\x1b')
+                read -rsn1 -t 0.1 tmp
+                        if [[ "$tmp" == "[" ]]; then
+                        read -rsn1 -t 0.1 tmp
+                        case "$tmp" in
+                        "A") echo "up";;
+                        "B") echo "down";;
+                        "C") echo "right";;
+                        "D") echo "left";;
+                        esac
+                        fi
+                        read -rsn5 -t 0.1
+                        ;;
+                q) break;;
+                esac
                 done
         }
-        bluescreen
+
+        case "$1" in
+        arrow)
+                arrow_input;;
+        esac
 }
 
+function menu
+
+function main {
+        function clear_screen {
+                colors=$(is_in $1)
+                for ((i=0; i<=lines; i++))
+                do
+                        printf "\033[$colors\033[K\033[0m\n"
+                done
+        }
+
+        index=50
+        tput cup 4 $index
+        
+        inputs arrow
+}
 main
-tput cup ${mid[0]} ${mid[1]}
-
-function user_input {
-        read -p $'\033[44m' test
-        printf "\033[0m"
-}
-
-user_input
